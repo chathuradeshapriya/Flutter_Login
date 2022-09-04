1ff672034cbb539b075339118a31587f8c748fbb
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-04 22:57:02 +0530

    Finished Login in page with connect API

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:api_call/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new SignUpScreen(),
+    );
+  }
+}
+
